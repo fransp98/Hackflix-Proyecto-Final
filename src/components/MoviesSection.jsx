3ce33bba06ccc435bc2e093cf7223289ed076c4e
fransp98 <fransilva1452@gmail.com>
2024-11-14 18:07:26 -0300

    jcc fix

--- src/components/MoviesSection.jsx
+++ src/components/MoviesSection.jsx
@@ -2,7 +2,7 @@ import Movie from "./Movie";
 
 function MovieSection({ movies }) {
   return (
-    <div className="movies-section container d-flex flex-wrap gap-3">
+    <div className="movies-section container justify-content-center d-flex flex-wrap gap-3">
       <Movie movies={movies} />
     </div>
   );
